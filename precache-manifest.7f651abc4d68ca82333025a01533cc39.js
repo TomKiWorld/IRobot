c1efcfdea3bf88f2b375caced4ed2b2e28a6c35b
TomKiDev <tomkidev@gmail.com>
2020-06-21 12:46:23 +0200

    Updates

---
+++ precache-manifest.7f651abc4d68ca82333025a01533cc39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb42371d1c45a3909855d495b186010f",
+    "url": "/IRobot/index.html"
+  },
+  {
+    "revision": "80a0e884709204d3e910",
+    "url": "/IRobot/static/css/2.50b8dffa.chunk.css"
+  },
+  {
+    "revision": "28af744e737e3725c11b",
+    "url": "/IRobot/static/css/main.3228af54.chunk.css"
+  },
+  {
+    "revision": "80a0e884709204d3e910",
+    "url": "/IRobot/static/js/2.70d797dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IRobot/static/js/2.70d797dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bc7ce1b370bd00c45e",
+    "url": "/IRobot/static/js/3.7c0c0d42.chunk.js"
+  },
+  {
+    "revision": "28af744e737e3725c11b",
+    "url": "/IRobot/static/js/main.f547febb.chunk.js"
+  },
+  {
+    "revision": "aee1b7a965f3697f17b8",
+    "url": "/IRobot/static/js/runtime-main.1de14709.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/IRobot/static/media/SEGA.12a3db8b.woff"
+  }
+]);
